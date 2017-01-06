fa68ade853f49f05d4959643d7b591f31fb0ddcb
Webster Lima <websterhlima@gmail.com>
2017-01-06 16:56:13 -0200

    commit

---
+++ statics/scripts/films-list.js
@@ -0,0 +1,27 @@
+var films = {
+    "1": {
+        "sinopse": "Woody the cowboy is young Andy\u2019s favorite toy. Yet this changes when Andy get the new super toy Buzz Lightyear for his birthday. Now that Woody is no longer number one he plans his revenge on Buzz. Toy Story is a milestone in film history for being the first feature film to use entirely computer animation.",
+        "titulo": "Toy Story (1995)",
+        "url_poster": "http://image.tmdb.org/t/p/original/uMZqKhT4YA6mqo2yczoznv7IDmv.jpg"
+    },
+    "3": {
+        "sinopse": "It's Ted the Bellhop's first night on the job...and the hotel's very unusual guests are about to place him in some outrageous predicaments. It seems that this evening's room service is serving up one unbelievable happening after another.",
+        "titulo": "Four Rooms (1995)",
+        "url_poster": "http://image.tmdb.org/t/p/original/eQs5hh9rxrk1m4xHsIz1w11Ngqb.jpg"
+    },
+    "2": {
+        "sinopse": "James Bond must unmask the mysterious head of the Janus Syndicate and prevent the leader from utilizing the GoldenEye weapons system to inflict devastating revenge on Britain.",
+        "titulo": "GoldenEye (1995)",
+        "url_poster": "http://image.tmdb.org/t/p/original/5c0ovjT41KnYIHYuF4AWsTe3sKh.jpg"
+    },
+    "5": {
+        "sinopse": "An agoraphobic psychologist and a female detective must work together to take down a serial killer who copies serial killers from the past.",
+        "titulo": "Copycat (1995)",
+        "url_poster": "http://image.tmdb.org/t/p/original/80czeJGSoik22fhtUM9WzyjUU4r.jpg"
+    },
+    "4": {
+        "sinopse": "A mobster travels to Hollywood to collect a debt and discovers that the movie business is much the same as his current job.",
+        "titulo": "Get Shorty (1995)",
+        "url_poster": "http://image.tmdb.org/t/p/original/vWtDUUgQAsVyvRW4mE75LBgVm2e.jpg"
+    }
+};
--- statics/scripts/main.js
+++ statics/scripts/main.js
@@ -3,53 +3,44 @@
 
     $(document).ready(function() {
         var filmsListTemplate = doT.template(getTemplate('statics/templates/films-list.html'));
-        
-        var data = {
-            films: [
-                {
-                    poster: "http://image.tmdb.org/t/p/original/uMZqKhT4YA6mqo2yczoznv7IDmv.jpg"
-                },
-                {
-                    poster: "http://image.tmdb.org/t/p/original/eQs5hh9rxrk1m4xHsIz1w11Ngqb.jpg"
-                },
-                {
-                    poster: "http://image.tmdb.org/t/p/original/5c0ovjT41KnYIHYuF4AWsTe3sKh.jpg"
-                },
-                {
-                    poster: "http://image.tmdb.org/t/p/original/80czeJGSoik22fhtUM9WzyjUU4r.jpg"
-                },
-                {
-                    poster: "http://image.tmdb.org/t/p/original/vWtDUUgQAsVyvRW4mE75LBgVm2e.jpg"
-                },
-                {
-                    poster: "http://image.tmdb.org/t/p/original/uMZqKhT4YA6mqo2yczoznv7IDmv.jpg"
-                },
-                {
-                    poster: "http://image.tmdb.org/t/p/original/eQs5hh9rxrk1m4xHsIz1w11Ngqb.jpg"
-                },
-                {
-                    poster: "http://image.tmdb.org/t/p/original/5c0ovjT41KnYIHYuF4AWsTe3sKh.jpg"
-                },
-                {
-                    poster: "http://image.tmdb.org/t/p/original/80czeJGSoik22fhtUM9WzyjUU4r.jpg"
-                },
-                {
-                    poster: "http://image.tmdb.org/t/p/original/vWtDUUgQAsVyvRW4mE75LBgVm2e.jpg"
-                },
-                {
-                    poster: "http://image.tmdb.org/t/p/original/uMZqKhT4YA6mqo2yczoznv7IDmv.jpg"
-                },
-                {
-                    poster: "http://image.tmdb.org/t/p/original/eQs5hh9rxrk1m4xHsIz1w11Ngqb.jpg"
-                }
-            ]
-        };
+        var filmInfoTemplate = doT.template(getTemplate('statics/templates/film-info.html'));
 
-        $('.recommendations').append(filmsListTemplate(data));
+        var userId = 101;
+
+        $.ajax({
+            headers: {
+                'authorization': 'Bearer MX3CV9fvZHGKtaT9rGC+tZty10iYAuzb5IPtLVdC1DYh2x2rSVBX0KT3nqwKxxeTOc7e43qK65RbvCOyu4amLgAvj+CnCkbevSlHjfShwaZnzMckX+vPsa2BskRwE9QfqngtqURdk2jiWEPe4dZqIojNkX+Y0SL33569W0nIx0g=.eyJ0aWQiOiAxfQ=='
+            },
+            contentType: 'application/json',
+            crossDomain: true,
+            url: 'https://recomendador-154618.appspot.com/api/services/recommendations?idx=' + userId + '&kind=user',
+            success: function(response) {
+                console.log(response);
+                if (response.status === 200) {
+                    var list = [];
+
+                    response.data.forEach(function(id) {
+                        list.push(films["" + id]);
+                    });
+
+                    list = list.filter(function(e) {
+                        return e.url_poster;
+                    });
+
+                    $('main').append(filmsListTemplate({'films': list}));
+                }
+            }
+        });
+
+        // $('main').html(filmInfoTemplate({'film': films['1']}));
 
         $('header .container-user').click(function() {
             $('header .user-list').toggle();
         });
+
+        $('main .recommendations-list li a').click(function() {
+            $('.recommendations').html(filmInfoTemplate({}));
+        })
     });
     
 })(jQuery);
