9c1171ff4fad11ca7b32e52b96277ab4b64804c4
Webster Lima <websterhlima@gmail.com>
2017-01-10 09:05:55 -0200

    Menu de seleção usuario

--- statics/scripts/main.js
+++ statics/scripts/main.js
@@ -1,21 +1,21 @@
 (function($) {
     'use strict';
 
-    $(document).ready(function() {
-        var filmsListTemplate = doT.template(getTemplate('statics/templates/films-list.html'));
-        var filmInfoTemplate = doT.template(getTemplate('statics/templates/film-info.html'));
+    var filmsListTemplate = doT.template(getTemplate('statics/templates/films-list.html'));
+    var filmInfoTemplate = doT.template(getTemplate('statics/templates/film-info.html'));
+    var headerUserTemplate = doT.template(getTemplate('statics/templates/header-user.html'));
+    var usersListTemplate = doT.template(getTemplate('statics/templates/users-list.html'));
 
-        var userId = 101;
-
+    function loadUserRecommendations(userId) {
         $.ajax({
+            method: 'GET',
             headers: {
-                'authorization': 'Bearer MX3CV9fvZHGKtaT9rGC+tZty10iYAuzb5IPtLVdC1DYh2x2rSVBX0KT3nqwKxxeTOc7e43qK65RbvCOyu4amLgAvj+CnCkbevSlHjfShwaZnzMckX+vPsa2BskRwE9QfqngtqURdk2jiWEPe4dZqIojNkX+Y0SL33569W0nIx0g=.eyJ0aWQiOiAxfQ=='
+                'Authorization': 'Bearer MX3CV9fvZHGKtaT9rGC+tZty10iYAuzb5IPtLVdC1DYh2x2rSVBX0KT3nqwKxxeTOc7e43qK65RbvCOyu4amLgAvj+CnCkbevSlHjfShwaZnzMckX+vPsa2BskRwE9QfqngtqURdk2jiWEPe4dZqIojNkX+Y0SL33569W0nIx0g=.eyJ0aWQiOiAxfQ=='
             },
-            contentType: 'application/json',
-            crossDomain: true,
             url: 'https://recomendador-154618.appspot.com/api/services/recommendations?idx=' + userId + '&kind=user',
             success: function(response) {
-                console.log(response);
+                response = JSON.parse(response);
+
                 if (response.status === 200) {
                     var list = [];
 
@@ -23,25 +23,53 @@
                         list.push(films["" + id]);
                     });
 
+
                     list = list.filter(function(e) {
                         return e.url_poster;
                     });
 
-                    $('main').append(filmsListTemplate({'films': list}));
+                    list = list.slice(0, 10);
+
+                    $('main').html(filmsListTemplate({'films': list}));
                 }
             }
         });
+    };
+
+    $(document).ready(function() {
+        var actualUser = users[0];
+
+        actualUser.selected = true;
+
+        $('header .user-area .container-user').html(headerUserTemplate({'user': actualUser}));
+
+        $('header .user-area').append(usersListTemplate({'users': users}));
+
+        $('header .user-area .container-user').click(function() {
+            $('header .user-area .user-list').toggle();
+        });
+
+        $('header .user-area .user-list li').click(function() {
+            $('.selected').removeClass('selected');
+            $(this).addClass('selected');
+
+            var userId = $(this).data('id');
+            
+            var user = users.filter(function(u) {
+                return u.id == userId;
+            })[0];
+
+            $('header .user-area .container-user').html(headerUserTemplate({'user': user}));
+            loadUserRecommendations(userId);
+        });
+
+        loadUserRecommendations(actualUser.id);
 
         // $('main').html(filmInfoTemplate({'film': films['1']}));
 
-        $('header .container-user').click(function() {
-            $('header .user-list').toggle();
-        });
-
         $('main .recommendations-list li a').click(function() {
             $('.recommendations').html(filmInfoTemplate({}));
-        })
+        });
     });
     
 })(jQuery);
-
---
+++ statics/scripts/users.js
@@ -0,0 +1,17 @@
+var users = [
+    {
+        'id': 101,
+        'name': 'Compadre Washington',
+        'photo_url': 'statics/images/compadre.jpg'
+    },
+    {
+        'id': 184,
+        'name': 'Carla Perez',
+        'photo_url': 'statics/images/carla.png'
+    },
+    {
+        'id': 227,
+        'name': 'Beto Jamaica',
+        'photo_url': 'statics/images/beto.jpg'
+    }
+];
