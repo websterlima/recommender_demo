37c795faef778781b965cd86cdb17ac35d921b8c
Everton Gago <everton.gjunior@gmail.com>
2017-01-12 12:53:09 -0200

    Set users profile

--- statics/scripts/users.js
+++ statics/scripts/users.js
@@ -1,16 +1,21 @@
+/*
+84 = Homem, 32 anos, executivo
+165= Mulher, 20 anos, não especificado
+179= Homem, 15 anos, estudante.
+*/
 var users = [
     {
-        'id': 235,
+        'id': 84,
         'name': 'Compadre Washington',
         'photo_url': 'statics/images/compadre.jpg'
     },
     {
-        'id': 184,
+        'id': 165,
         'name': 'Carla Perez',
         'photo_url': 'statics/images/carla.png'
     },
     {
-        'id': 107,
+        'id': 179,
         'name': 'Beto Jamaica',
         'photo_url': 'statics/images/beto.jpg'
     }
